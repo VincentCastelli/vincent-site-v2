463beb227487695486f79ab236a03880adf58591
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2025-06-25 00:37:11 -0700

    Init commit for portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="container mx-auto max-w-7xl">
+        <Navbar />
+        {/* Hero */}
+        {/* About */}
+        {/* Experience */}
+        {/* Contact */}
+        {/* Footer */}
+      </div>
+    </>
+  );
+};
+
+export default App;
