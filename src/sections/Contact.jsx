7e49ee1b748634f7ff39b2a07a65e6e32965dd13
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2025-06-30 15:50:06 -0700

    Init commit for Contact section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,7 +1,129 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+// import Alert from "../components/Alert";
+// import { Particles } from "../components/Particles";
+
 const Contact = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  //   const [showAlert, setShowAlert] = useState(false);
+  //   const [alertType, setAlertType] = useState("success");
+  //   const [alertMessage, setAlertMessage] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  //   const showAlertMessage = (type, message) => {
+  //     setAlertType(type);
+  //     setAlertMessage(message);
+  //     setShowAlert(true);
+  //     setTimeout(() => {
+  //       setShowAlert(false);
+  //     }, 5000);
+  //   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await emailjs.send(
+        "service_79b0nyj",
+        "template_17us8im",
+        {
+          from_name: formData.name,
+          to_name: "Ali",
+          from_email: formData.email,
+          to_email: "AliSanatiDev@gmail.com",
+          message: formData.message,
+        },
+        "pn-Bw_mS1_QQdofuV"
+      );
+      setIsLoading(false);
+      setFormData({ name: "", email: "", message: "" });
+      //   showAlertMessage("success", "You message has been sent!");
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      // showAlertMessage("danger", "Somthing went wrong!");
+    }
+  };
+
   return (
-    <section id="contact" className="c-space section-spacing">
-      <h2 className="text-heading">Contact Me</h2>
+    <section id="contact" className="relative flex c-space section-spacing">
+      <div className="flex flex-col items-center md:max-w-3/5 px-10 py-10 mx-auto border border-white/10 rounded-2xl bg-bg-dark">
+        <div className="flex flex-col items-start w-full gap-5 mb-10">
+          <h2 className="font-bold text-3xl md:text-4xl text-bg-light">
+            Contact Me
+          </h2>
+          <p className="font-normal text-neutral-400">
+            Interested in collaborating or have a question? Feel free to reach
+            out! You can contact me via email or connect with me on LinkedIn.
+          </p>
+        </div>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label htmlFor="name" className="feild-label">
+              Full Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              className="field-input field-input-focus"
+              placeholder="John Doe"
+              autoComplete="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="feild-label">
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="field-input field-input-focus"
+              placeholder="JohnDoe@email.com"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="message" className="feild-label">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              type="text"
+              rows="4"
+              className="field-input field-input-focus"
+              placeholder="Share your thoughts..."
+              autoComplete="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-1 py-3 text-lg text-center rounded-md cursor-pointer bg-radial from-lavender to-royal hover-animation"
+          >
+            {!isLoading ? "Send" : "Sending..."}
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
